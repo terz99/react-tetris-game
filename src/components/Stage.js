a589f3cccd13182ce531f7cbf02ef1dba4aa81a0
Dushan Terzikj <dusanterzic87@gmail.com>
2020-07-10 15:09:15 +0200

    Add tetrominos

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -2,9 +2,19 @@ import React from "react";
 import Cell from "./Cell";
 
 function Stage(props) {
+  console.log(props.stage);
   return (
     <div>
-      <Cell/>
+      {props.stage.map(row =>
+        row.map((cell, x) => {
+          return (
+            <Cell
+              key={x}
+              type={cell[0]}
+            />
+          );
+        })
+      )}
     </div>
   );
 }
